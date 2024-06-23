2de033b4e7a313746f0a8274a28ffddb3955bd37
Your Name <your_email@whatever.com>
2024-06-23 17:17:25 +0500

    Initial Commit

---
+++ ChitChat/ChitChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChitChatApp.swift
+//  ChitChat
+//
+//  Created by Бакдаулет Турсунбаев on 23.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChitChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
